5e2190e804487bac446510ed7a08ee5b2bd009d2
BorlandDev <minsk1115596@gmail.com>
2022-07-04 16:33:53 +0300

    Refactoring

--- app/src/main/java/com/borlanddev/natife_fourth/MainActivity.kt
+++ app/src/main/java/com/borlanddev/natife_fourth/MainActivity.kt
@@ -3,17 +3,20 @@ package com.borlanddev.natife_fourth
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.observe
 import androidx.recyclerview.widget.GridLayoutManager
 import com.borlanddev.natife_fourth.adapter.ItemAdapter
 import com.borlanddev.natife_fourth.databinding.ActivityMainBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableObserver
 import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,8 +33,8 @@ class MainActivity : AppCompatActivity() {
         itemAdapter = ItemAdapter()
 
         //observeWithLiveData()
-        //observeWithRx(rx)
-        observeWithCoroutines()
+        observeWithRx(rx)
+        //observeWithCoroutines(this)
 
         binding?.recyclerView?.layoutManager = GridLayoutManager(this, 3)
         binding?.recyclerView?.adapter = itemAdapter
@@ -39,40 +42,41 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        compositeDisposable.dispose()
 
+        compositeDisposable.dispose()
     }
 
-
     private fun observeWithLiveData() {
-        mainVM.genRandomNumberWithThread()
-        mainVM.numbersFromLiveData.observe(this) {
+        mainVM.genRandomNumber()
+        mainVM.numbers.observe(this) {
             itemAdapter?.addNumbers(it)
         }
     }
 
     private fun observeWithRx(rx: Rx) {
-        rx.source
+        compositeDisposable.add(rx.source
             .subscribeOn(Schedulers.newThread())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({
+            .subscribe ({
                 itemAdapter?.addNumbers(it)
             }, {
                 it.printStackTrace()
-            })
-    }
+            }))
+          }
 
-    private fun observeWithCoroutines() {
-        mainVM.genRandomNumberWithFlow()
+    private fun observeWithCoroutines(viewLifeCycleOwner: AppCompatActivity) {
         val list = mutableListOf<Int>()
 
-        this.lifecycleScope.launch(Dispatchers.Main) {
-            this@MainActivity.repeatOnLifecycle(Lifecycle.State.STARTED) {
+        viewLifeCycleOwner.lifecycleScope.launch(Dispatchers.Main) {
 
-                mainVM.numbersFromFlow.collect {
-                    list.add(it)
-                    itemAdapter?.addNumbers(list)
+            flow {
+                while (true) {
+                    emit(Random.nextInt(0, 100))
+                    delay(2000)
                 }
+            }.collect {
+                list.add(it)
+                itemAdapter?.addNumbers(list)
             }
         }
     }
--- app/src/main/java/com/borlanddev/natife_fourth/MainViewModel.kt
+++ app/src/main/java/com/borlanddev/natife_fourth/MainViewModel.kt
@@ -3,49 +3,29 @@ package com.borlanddev.natife_fourth
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlin.concurrent.thread
 import kotlin.random.Random
 
 class MainViewModel : ViewModel() {
-    private val _numbersFromLiveData = MutableLiveData<List<Int>>()
-    val numbersFromLiveData: LiveData<List<Int>> = _numbersFromLiveData
+    private val _numbers = MutableLiveData<List<Int>>()
+    val numbers: LiveData<List<Int>> = _numbers
     private val list = mutableListOf<Int>()
     private var myThread: Thread? = null
-    val numbersFromFlow = MutableStateFlow(0)
 
-
-    fun genRandomNumberWithThread() {
+    fun genRandomNumber() {
         myThread = thread {
-            while (true) {
+
+            while (myThread?.isInterrupted != true) {
                 list.add(Random.nextInt(0, 100))
                 Thread.sleep(2000)
-                _numbersFromLiveData.postValue(list)
+                _numbers.postValue(list)
             }
         }
     }
-
-
-    fun genRandomNumberWithFlow() {
-        viewModelScope.launch {
-
-            withContext(Dispatchers.Default) {
-                while (true) {
-                    delay(2000)
-                    numbersFromFlow.value = Random.nextInt(0, 100)
-                }
-            }
-        }
-    }
-
 
     override fun onCleared() {
         super.onCleared()
-        myThread?.stop()
+        myThread?.interrupt()
     }
 }
